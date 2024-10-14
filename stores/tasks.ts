f066ee1ce1b4212449cd45df3d9fd9aba91da6e6
Mathis Bryan Kengni <skymkengni1@gmail.com>
2024-10-14 21:59:38 +0200

    task pinia store

---
+++ stores/tasks.ts
@@ -0,0 +1,62 @@
+import type { ObjectId } from "mongoose";
+import { defineStore } from "pinia";
+import type { TaskDocument} from "~/server/models/Task";
+
+export const useTasksStore = defineStore("tasks", () => {
+    const { data } = useAuth();
+    const user = (data.value?.user as any)._id;
+    const tasks = ref<TaskDocument[]>([]);
+
+    async function fetch() {
+        const response: TaskDocument[] = await $fetch("/api/tasks", {
+            method: "GET",
+        });
+
+        if (response) {
+            tasks.value = response;
+        }
+    }
+
+    async function create(task: TaskDocument) {
+        const response: TaskDocument = await $fetch("/api/tasks", {
+            method: "POST",
+            body: task,
+        });
+
+        if (response) {
+            return response;
+        }
+    }
+
+    async function update(task: TaskDocument) {
+        const response: TaskDocument = await $fetch(`/api/tasks`, {
+            method: "PUT",
+            body: task,
+        });
+
+        if (response) {
+            return response;
+        }
+    }
+
+    async function remove(id: ObjectId) {
+        const response: TaskDocument = await $fetch(`/api/tasks/${id}`, {
+            method: "DELETE",
+        });
+
+        if (response) {
+            return response;
+        }
+    }
+
+    const get = computed(() => tasks.value);
+
+    return {
+        tasks,
+        fetch,
+        get,
+        create,
+        update,
+        remove,
+    };
+})
