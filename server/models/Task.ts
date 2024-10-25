d53291b1783ee4b973fbbb32f5c1ee01f102608e
Mathis Bryan Kengni <skymkengni1@gmail.com>
2024-10-25 18:25:17 +0200

    refactor

--- server/models/Task.ts
+++ server/models/Task.ts
@@ -1,50 +1,50 @@
-import {Schema, model, ObjectId, Document} from "mongoose";
+import { Schema, model, ObjectId, Document } from "mongoose";
 
 export interface TaskDocument extends Document {
-    _id: ObjectId;
-    title: string;
-    description: string;
-    status: "Backlog" | "Working On" | "Done";
-    priority: "Low" | "Medium" | "High";
-    course: string;
-    link: string;
-    dueDate: Date;
-    createdAt: Date;
-    updatedAt: Date;
+  _id: ObjectId;
+  title: string;
+  description: string;
+  status: "Backlog" | "Working On" | "Done";
+  priority: "Low" | "Medium" | "High";
+  course: string;
+  link: string;
+  dueDate: Date;
+  createdAt: Date;
+  updatedAt: Date;
 }
 
 const taskSchema = new Schema(
-    {
-        title: {
-            type: String,
-            required: [true, "Title is required"],
-        },
-        description: {
-            type: String,
-        },
-        status: {
-            type: String,
-            enum: ["Backlog", "Working On", "Done"],
-            default: "Backlog",
-        },
-        priority: {
-            type: String,
-            enum: ["Low", "Medium", "High"],
-            default: "Medium",
-        },
-        course: {
-            type: String,
-        },
-        link: {
-            type: String,
-        },
-        dueDate: {
-            type: Date,
-        },
+  {
+    title: {
+      type: String,
+      required: [true, "Title is required"],
     },
-    {
-        timestamps: true,
-    }
+    description: {
+      type: String,
+    },
+    status: {
+      type: String,
+      enum: ["Backlog", "Working On", "Done"],
+      default: "Backlog",
+    },
+    priority: {
+      type: String,
+      enum: ["Low", "Medium", "High"],
+      default: "Medium",
+    },
+    course: {
+      type: String,
+    },
+    link: {
+      type: String,
+    },
+    dueDate: {
+      type: Date,
+    },
+  },
+  {
+    timestamps: true,
+  },
 );
 
 export const Task = model<TaskDocument>("Task", taskSchema);
