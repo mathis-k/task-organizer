329f47376a1e20eb3aa1f07f681b96058956d320
Mathis Bryan Kengni <skymkengni1@gmail.com>
2025-03-26 16:03:08 +0100

    Update Navbar.vue

--- components/Navbar.vue
+++ components/Navbar.vue
@@ -1,6 +1,4 @@
 <script setup lang="ts">
-import { ListTodo } from "lucide-vue-next";
-
 const { data } = useAuth();
 const isCollapsed = ref(true);
 </script>
@@ -8,20 +6,7 @@ const isCollapsed = ref(true);
 <template>
   <div v-if="data?.user" :data-collapsed="isCollapsed">
     <nav class="bg-background border-b border-border py-4">
-      <div class="container mx-auto flex items-center justify-between px-6">
-        <div class="flex items-center space-x-4">
-          <ListTodo class="w-8 h-8 text-foreground" />
-          <span class="text-xl font-bold text-foreground">
-            Mathis' Task Organizer
-          </span>
-        </div>
-
-        <div class="flex items-center space-x-6">
-          <CreateTask />
-          <Links />
-          <User />
-        </div>
-      </div>
+      <User />
     </nav>
   </div>
 </template>
