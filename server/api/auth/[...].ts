681d279d559c7dcbf5c2f25eacc47da9304eff31
Mathis Bryan Kengni <skymkengni1@gmail.com>
2025-03-29 22:47:12 +0100

    Update [...].ts

--- server/api/auth/[...].ts
+++ server/api/auth/[...].ts
@@ -4,66 +4,65 @@ import { User } from "~/server/models/User";
 import bcrypt from "bcrypt";
 
 export default NuxtAuthHandler({
-    secret: useRuntimeConfig().authSecret,
-    pages: {
-        signIn: "/login",
+  secret: useRuntimeConfig().authSecret,
+  pages: {
+    signIn: "/login",
+  },
+  providers: [
+    // @ts-expect-error
+    CredentialsProvider.default({
+      name: "Credentials",
+      credentials: {},
+
+      async authorize(credentials: { email: string; password: string }) {
+        const user = await User.findOne({ email: credentials.email });
+
+        if (!user) {
+          throw createError({
+            statusCode: 401,
+            statusMessage: "Unauthorized",
+            message: "You entered invalid credentials.",
+          });
+        }
+
+        const isValid = await bcrypt.compare(
+          credentials.password,
+          user.password,
+        );
+
+        if (!isValid) {
+          throw createError({
+            statusCode: 401,
+            statusMessage: "Unauthorized",
+            message: "You entered an invalid password.",
+          });
+        }
+
+        if (isValid) {
+          return { ...user.toObject(), password: undefined };
+        }
+      },
+    }),
+  ],
+  session: {
+    strategy: "jwt",
+  },
+  callbacks: {
+    async jwt({ token, user }) {
+      if (user) {
+        token = {
+          ...token,
+          ...user,
+        };
+      }
+      return token;
     },
-    providers: [
-        // @ts-expect-error
-        CredentialsProvider.default({
-            name: "Credentials",
-            credentials: {},
-
-            async authorize(credentials: { email: string; password: string }) {
-                console.log("credentials", credentials);
-                const user = await User.findOne({ email: credentials.email });
-
-                if (!user) {
-                    throw createError({
-                        statusCode: 401,
-                        statusMessage: "Unauthorized",
-                        message: "You entered invalid credentials.",
-                    });
-                }
-
-                const isValid = await bcrypt.compare(
-                    credentials.password,
-                    user.password
-                );
-
-                if (!isValid) {
-                    throw createError({
-                        statusCode: 401,
-                        statusMessage: "Unauthorized",
-                        message: "You entered an invalid password.",
-                    });
-                }
-
-                if (isValid) {
-                    return { ...user.toObject(), password: undefined };
-                }
-            },
-        }),
-    ],
-    session: {
-        strategy: "jwt",
+    async session({ session, token }) {
+      session.user = {
+        ...token,
+        ...session.user,
+      };
+      return session;
     },
-    callbacks: {
-        async jwt({ token, user }) {
-            if (user) {
-                token = {
-                    ...token,
-                    ...user,
-                };
-            }
-            return token;
-        },
-        async session({ session, token }) {
-            session.user = {
-                ...token,
-                ...session.user,
-            };
-            return session;
-        },
-    },
+  },
 });
